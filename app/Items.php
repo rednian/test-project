8d0b1c40d4d3de8af71a186a041a193b34624e4d
Don del Rosario <dondelrosario93@gmail.com>
2020-09-17 20:21:29 +0400

    changes in model

--- app/Items.php
+++ app/Items.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Items extends Model
 {
+    protected $table = 'items';
     protected $primaryKey ='item_id';
     protected $fillable = ['item_name', 'is_selected'];
     
